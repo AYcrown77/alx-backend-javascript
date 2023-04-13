95ab84519613f855b085fe7d86e20b6289c5099e
AYcrown77 <aycrown77@gmail.com>
2023-04-13 10:06:47 +0000

    Fix the error

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,12 @@
-export default function createIteratorobject(report) {
-  const myarray = [];
-  for (const item of Object.values(report.allEmployees)) {
-    myarray.push(...item);
+export default function createIteratorObject(report) {
+  const employeeIter = [];
+
+  for (const key in report.allEmployees) {
+    if ({}.hasOwnProperty.call(report.allEmployees, key)) {
+      for (const employee of report.allEmployees[key]) {
+        employeeIter.push(employee);
+      }
+    }
   }
-  
-  return myarray;
+  return employeeIter;
 }
